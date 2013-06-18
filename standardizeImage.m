9c59181a5ae2f0ba5eb7c4956e41121a7314e80c
Andrea Vedaldi <vedaldi@gmail.com>
2013-06-18 16:21:36 +0100

    standardizeImge.m: more flexible

--- standardizeImage.m
+++ standardizeImage.m
@@ -5,5 +5,14 @@ function im = standardizeImage(im)
 
 % Author: Andrea Vedaldi
 
+if isstr(im)
+  if exist(im, 'file')
+    fullPath = im ;
+  else
+    fullPath = fullfile('data','images',[im '.jpg']) ;
+  end
+  im = imread(fullPath) ;
+end
+
 im = im2single(im) ;
 if size(im,1) > 480, im = imresize(im, [480 NaN]) ; end
