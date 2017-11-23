71efcc878af48d158b8e1346cbb7cbe847e58acc
Andrea Vedaldi <vedaldi@gmail.com>
2017-11-23 20:23:31 +0000

    starts removing VLFeat, updating MCN

--- extra/download.sh
+++ extra/download.sh
@@ -9,19 +9,19 @@ mkdir -p data/cnn
 wget http://www.vlfeat.org/sandbox-matconvnet/models/imagenet-vgg-m-128.mat \
     --output-document=data/cnn/imagenet-vgg-m-128.mat --continue
 
-if test ! -e vlfeat
-then
-    wget http://www.vlfeat.org/download/vlfeat-0.9.20-bin.tar.gz --output-document=data/vlfeat-0.9.20-bin.tar.gz --continue
-    tar xzvf data/vlfeat-0.9.20-bin.tar.gz
-    mv vlfeat-0.9.20 vlfeat
-fi
+# if test ! -e vlfeat
+# then
+#     wget http://www.vlfeat.org/download/vlfeat-0.9.20-bin.tar.gz --output-document=data/vlfeat-0.9.20-bin.tar.gz --continue
+#     tar xzvf data/vlfeat-0.9.20-bin.tar.gz
+#     mv vlfeat-0.9.20 vlfeat
+# fi
 
 if test ! -e matconvnet
 then
-    wget http://www.vlfeat.org/sandbox-matconvnet/download/matconvnet-1.0-beta17.tar.gz \
+    wget http://www.vlfeat.org/sandbox-matconvnet/download/matconvnet-1.0-beta25.tar.gz \
          --output-document=data/matconvnet.tar.gz --continue
     tar xzvf data/matconvnet.tar.gz
-    mv matconvnet-1.0-beta23 matconvnet
+    mv matconvnet-1.0-beta25 matconvnet
 fi
 
 # Download images
--- setup.m
+++ setup.m
@@ -1,8 +1,25 @@
+function setup(varargin)
 % SETUP  Add the required search paths to MATLAB
-if exist('vl_version') ~= 3, run('vlfeat/toolbox/vl_setup') ; end
-if exist('vl_nnconv') ~= 3
-  run('matconvnet/matlab/vl_setupnn') ;
-  if exist('vl_nnconv') ~= 3
-    vl_compilenn() ;
+run matconvnet/matlab/vl_setupnn ;
+addpath matconvnet/examples ;
+
+opts.useGpu = false ;
+opts.verbose = false ;
+opts.enableImReadJPEG = false ;
+opts = vl_argparse(opts, varargin) ;
+
+try
+  vl_nnconv(single(1),single(1),[]) ;
+catch
+  warning('VL_NNCONV() does not seem to be compiled. Trying to compile it now.') ;
+  vl_compilenn('enableGpu', opts.useGpu, 'verbose', opts.verbose) ;
+end
+
+if opts.useGpu
+  try
+    vl_nnconv(gpuArray(single(1)),gpuArray(single(1)),[]) ;
+  catch
+    vl_compilenn('enableGpu', opts.useGpu, 'verbose', opts.verbose) ;
+    warning('GPU support does not seem to be compiled in MatConvNet. Trying to compile it now') ;
   end
 end
